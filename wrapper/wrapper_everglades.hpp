938aab512d48e3b27064513a13343e89aa80ef88
bonkoskk <bonkoski.kevin@gmail.com>
2016-01-05 14:17:40 +0100

    Toto
    Signed-off-by: bonkoskk <bonkoski.kevin@gmail.com>

---
+++ wrapper/wrapper_everglades.hpp
@@ -0,0 +1,20 @@
+/*#pragma once
+
+#include "everglades.hpp"
+
+using namespace System;
+
+namespace Wrapper {
+
+	public ref class WrapperClassEverglades
+	{
+	private:
+		double mVLR;
+		//gsl_matrix path;
+	public:
+		double get_payoff();
+		WrapperClassEverglades(double VLR);
+		~WrapperClassEverglades();
+	};
+
+}*/
