947f574374dc83807f3bec9392c786c116405632
lobamba <lobamba@bitbucket.org>
2016-01-05 20:43:54 +0100

    parser

--- Everglades/Controllers/HomeController.cs
+++ Everglades/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using APIFiMag;
+using APIFiMag.Datas;
+using APIFiMag.Exporter;
+using APIFiMag.Importer;
 
 namespace Everglades.Controllers
 {
@@ -16,6 +20,21 @@ namespace Everglades.Controllers
 
         public static bool Yahoo_Finance_Parsing()
         {
+          
+            List<string> symbol = new List<string>() { "EDF.PA", "EN.PA", "BNP.PA", "1398.HK", "JNJ",  "MSFT","NESNE.SW","NOVNEE.SW","PG","ROG.VX" };
+            List<HistoricalColumn> columns = new List<HistoricalColumn>();
+            columns.Add(HistoricalColumn.Close);
+            columns.Add(HistoricalColumn.High);
+            columns.Add(HistoricalColumn.Open);
+            columns.Add(HistoricalColumn.Volume);
+            columns.Add(HistoricalColumn.Low);
+            DateTime debut = new DateTime(2015, 12, 06);
+            DateTime fin = new DateTime(2015, 12, 08);
+            DataActif donnees = new DataActif(symbol, columns, debut, fin);
+            Environment.CurrentDirectory = "C:/Users/ensimag/Desktop/peps/peps/Everglades/bin";
+            donnees.ImportData(new ImportYahoo());
+            donnees.Export(new ExportJSON("donnees.json"));
+            //System.Console.WriteLine(donnees.ToString());
             return false;
         }
 
