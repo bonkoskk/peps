3360d8f497313a5c461005ec298fb0960a121a3a
josib <bla@bla.com>
2016-01-05 20:15:10 +0100

    petite interface sympa oklm

---
+++ Everglades/Models/Everglades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Everglades.Models
+{
+    public class Everglades : IAsset
+    {
+
+        string IAsset.Get_Name()
+        {
+            return "Everglades product";
+        }
+
+        double IAsset.Get_Price()
+        {
+            throw new NotImplementedException();
+        }
+
+        double[] IAsset.Get_Price(DateTime t1, DateTime t2, TimeStep step)
+        {
+            throw new NotImplementedException();
+        }
+
+        double IAsset.Get_Price(DateTime t)
+        {
+            throw new NotImplementedException();
+        }
+
+        double IAsset.Get_Delta(DateTime t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
