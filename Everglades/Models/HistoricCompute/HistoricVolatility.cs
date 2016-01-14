56d5f3a9e1ed08390c357b5de7c5f5536cd76640
Josi Baptiste <baptiste.josi@laposte.net>
2016-01-14 17:39:55 +0100

    blabla et bla et HistoricCompute

---
+++ Everglades/Models/HistoricCompute/HistoricVolatility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Everglades.Models.HistoricCompute
+{
+    public class HistoricVolatility
+    {
+        internal static double compute(int date_nb, double[] prices)
+        {
+            double sum = 0;
+            double sum2 = 0;
+            for (int i = 0; i < date_nb; i++)
+            {
+                sum += prices[i];
+                sum2 += prices[i] * prices[i];
+            }
+            double mean = sum / date_nb;
+            double var = ( sum2 - date_nb*mean*mean ) / (date_nb - 1);
+            return Math.Sqrt(var);
+        }
+    }
+}
