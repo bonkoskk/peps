d4ce9fdd55a901ed6324d4df50a41d7a8f234c0b
Josi Baptiste <baptiste.josi@laposte.net>
2016-03-25 15:49:00 +0100

    img deleted

--- Everglades/Models/ModelManage.cs
+++ Everglades/Models/ModelManage.cs
@@ -38,7 +38,8 @@ namespace Everglades.Models
             qpcptfaw db = new qpcptfaw();
             //Access.Clear_Everglades_Prices();
             DBInitialisation.DBInit(db);
-            HistoricCompute.RiskMetrics.initRiskMetrics();
+            //HistoricCompute.RiskMetrics.initRiskMetrics();
+            
             timers.stop("Database initialization");
             /*
             for (int i = 1; i < 50 ; i++ )
